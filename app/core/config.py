c40f085105ef591d3c926be6224b4c884e9dde4e
kimjy-par <kimjy.par@gmail.com>
2022-10-10 20:45:36 +0900

    Feat: replace MySQL to PostgreSQL

--- app/core/config.py
+++ app/core/config.py
@@ -5,18 +5,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Setting():
-    MYSQL_USER = os.getenv('MYSQL_USER')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
-    MYSQL_HOST = os.getenv('MYSQL_HOST')
-    MYSQL_PORT = os.getenv('MYSQL_PORT')
-    MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
+    PSQL_USER = os.getenv('PSQL_USER')
+    PSQL_PASSWORD = os.getenv('PSQL_PASSWORD')
+    PSQL_HOST = os.getenv('PSQL_HOST')
+    PSQL_PORT = os.getenv('PSQL_PORT')
+    PSQL_DATABASE = os.getenv('PSQL_DATABASE')
 
-    DATABASE_URI='mysql+pymysql://{user}:{password}@{host}:{port}/{database}'.format(
-        user=MYSQL_USER, 
-        password=MYSQL_PASSWORD,
-        host=MYSQL_HOST,
-        port=MYSQL_PORT,
-        database=MYSQL_DATABASE,        
+    DATABASE_URI='postgresql://{user}:{password}@{host}:{port}/{database}'.format(
+        user=PSQL_USER, 
+        password=PSQL_PASSWORD,
+        host=PSQL_HOST,
+        port=PSQL_PORT,
+        database=PSQL_DATABASE,        
     )
 
 settings = Setting()
